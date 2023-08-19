7bc68e948d59b30e341f850032125ccbf4e4250d
k1pp0 <firststepkk@gmail.com>
2023-08-19 21:25:02 +0900

    feat: aws lambda powertools

--- README.md
+++ README.md
@@ -18,3 +18,47 @@ Using default provider  awscloudformation
 ? secretAccessKey:  ****************************************
 ? region:  us-east-1
 ```
+
+## amplify add api
+```
+? Select from one of the below mentioned services: REST
+✔ Provide a friendly name for your resource to be used as a label for this category in the project: · v1
+✔ Provide a path (e.g., /book/{isbn}): · /api/v1
+Only one option for [Choose a Lambda source]. Selecting [Create a new Lambda function].
+? Provide an AWS Lambda function name: v1
+? Choose the runtime that you want to use: Python
+Only one template found - using Hello World by default.
+
+✅ Available advanced settings:
+- Resource access permissions
+- Scheduled recurring invocation
+- Lambda layers configuration
+- Environment variables configuration
+- Secret values configuration
+
+? Do you want to configure advanced settings? No
+? Do you want to edit the local lambda function now? No
+```
+
+## amplify update function
+```
+? Select the Lambda function you want to update v1
+General information
+- Name: v1
+- Runtime: python
+
+Resource access permission
+- Not configured
+
+Scheduled recurring invocation
+- Not configured
+
+Lambda layers
+- arn:aws:lambda:us-east-1:017000801446:layer:AWSLambdaPowertoolsPythonV2:40
+
+Environment variables:
+- TZ: Asia/Tokyo
+
+Secrets configuration
+- Not configured
+```
--- amplify/backend/function/v1/src/index.py
+++ amplify/backend/function/v1/src/index.py
@@ -1,15 +1,37 @@
 import json
+from http import HTTPStatus
 
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.event_handler import (
+    APIGatewayRestResolver,
+    Response,
+    content_types,
+)
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.logging import correlation_paths
+
+
+app = APIGatewayRestResolver()
+logger = Logger(service='playground', use_rfc3339=True) 
+
+@app.get("/api/v1/todos")
+def get_todos() -> dict:
+  result: dict = json.dumps('Hello from your new Amplify Python lambda!')
+  response: Response = Response(
+    status_code=HTTPStatus.OK.value,
+    content_type=content_types.APPLICATION_JSON,
+    body=result
+  )
+  return response
+
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+  logger.info(event)
+  response: dict = app.resolve(event, context)
+  response['headers'] = {
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
   }
+  logger.info(response)
+  return response
